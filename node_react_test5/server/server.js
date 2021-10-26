6fcf9819b34adc43758e8e80b74f0951ec6ee369
everglow <78077569+okchanho@users.noreply.github.com>
2021-10-26 20:38:36 +0900

    concurrently
    backend frontend 동시에 start

--- node_react_test5/server/server.js
+++ node_react_test5/server/server.js
@@ -5,6 +5,11 @@ app.get('/', (req, res)=>{
     res.send('hello');
 })
 
+app.get('/api/hello', (req, res)=>{
+    res.send('안녕하세요~')
+})
+
+
 app.listen(port, ()=>{
     console.log('Express Server Started On', port)
 });
