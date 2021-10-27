f4b64557bd5b7900524c504c212094a23c5a9dde
everglow <78077569+okchanho@users.noreply.github.com>
2021-10-27 22:44:01 +0900

    React JS #7 state, useState - 초보자를 위한 리액트 강좌

---
+++ Coding_Angma_Class/Handing_Event/README.md
@@ -0,0 +1,19 @@
+# Coding_ANGMA React Class
+> 코딩악마 React Class
+> 해당 웹페이지에서 확인하세요.
+
+![](../header.png)
+
+## 설치 방법
+
+```sh
+npm install
+```
+
+## 업데이트 내역
+
+* 10/27
+    * Handling Events
+ 
+## 정보
+[coding_angma](https://www.youtube.com/channel/UCxft4RZ8lrK_BdPNz8NOP7Q)
---
+++ Coding_Angma_Class/State_useState/client/src/Hello.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+function Hello() {
+    // let name = 'Mike';
+    const [name, setName] = useState('Mike');
+
+    function changeName() {
+        const newName = name === "Mike" ? "Jane" : "Mike"
+        console.log(name);
+        // document.getElementById("name").innerText = name;
+        setName(newName)
+    }
+
+
+    return (
+        <div>
+            <h1>state</h1>
+            <h2 id="name">{name}</h2>
+            <button onClick={changeName}>Change</button>
+        </div>
+    )
+}
+
+export default Hello
