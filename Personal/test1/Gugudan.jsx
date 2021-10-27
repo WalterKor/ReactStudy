99cabe0363e417d927e55e7986f51513c711b93d
everglow <78077569+okchanho@users.noreply.github.com>
2021-10-27 19:41:23 +0900

    10/27
    10/27

--- Personal/test1/Gugudan.jsx
+++
@@ -1,5 +0,0 @@
-const React = require('react');
-const { render } = require('react-dom');
-const { usestate, useRef } = React;
-
-module.exports = Gugudan;
--- node_react_test6/client/src/_reducer/user_reducer.js
+++ node_react_test6/client/src/_reducer/user_reducer.js
@@ -8,6 +8,10 @@ export default function (state = {}, action) {
         case LOGIN_USER:
             return {...state, loginSuccess: action.payload}
             break;
+
+        case REGISTER_USER:
+            return {...state, register: action.payload}
+            break;
     
         default:
             return state;
