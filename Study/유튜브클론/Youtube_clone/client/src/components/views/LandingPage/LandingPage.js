e7268ff379decafb771059daed762b353e4187c4
everglow <78077569+okchanho@users.noreply.github.com>
2021-12-02 22:04:32 +0900

    a tag

--- Study/유튜브클론/Youtube_clone/client/src/components/views/LandingPage/LandingPage.js
+++ Study/유튜브클론/Youtube_clone/client/src/components/views/LandingPage/LandingPage.js
@@ -28,21 +28,25 @@ function LandingPage() {
         var seconds = Math.floor(video.duration - minutes * 60);
 
         return <Col lg={6} md={8} xs={24}>
-            <div style={{ position: 'relative' }}>
-                <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`} />
-                <div className=" duration"
-                    style={{ bottom: 0, right:0, position: 'absolute', margin: '4px', 
-                    color: '#fff', backgroundColor: 'rgba(17, 17, 17, 0.8)', opacity: 0.8, 
-                    padding: '2px 4px', borderRadius:'2px', letterSpacing:'0.5px', fontSize:'12px',
-                    fontWeight:'500', lineHeight:'12px' }}>
-                    <span>{minutes} : {seconds}</span>
+            <a href={`/video/post/${video._id}`}>
+                <div style={{ position: 'relative' }}>
+                    <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`} />
+                    <div className=" duration"
+                        style={{ bottom: 0, right:0, position: 'absolute', margin: '4px', 
+                        color: '#fff', backgroundColor: 'rgba(17, 17, 17, 0.8)', opacity: 0.8, 
+                        padding: '2px 4px', borderRadius:'2px', letterSpacing:'0.5px', fontSize:'12px',
+                        fontWeight:'500', lineHeight:'12px' }}>
+                        <span>{minutes} : {seconds}</span>
+                    </div>
                 </div>
-            </div><br />
+            </a>
+            <br />
             <Meta
                 avatar={
                     <Avatar src={video.writer.image} />
                 }
                 title={video.title}
+                description=""
             />
             <span>{video.writer.name} </span><br />
             <span style={{ marginLeft: '3rem' }}> {video.views}</span>
