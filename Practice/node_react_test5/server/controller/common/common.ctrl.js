8f1f71e70f97080226cd94a57911137062f63863
Walter <78077569+WalterKor@users.noreply.github.com>
2022-02-18 13:14:28 +0900

    setting

--- Practice/node_react_test5/server/controller/common/common.ctrl.js
+++
@@ -1,3 +0,0 @@
-exports.get_Hello = (req, res)=>{
-    res.send('hello common');
-}
--- Practice/node_react_test5/server/controller/index.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const router = Router()
-
-/*router 설정해주기*/
-router.use('/common', require('./common'));
-
-module.exports = router;
